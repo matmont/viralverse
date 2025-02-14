0c34f10f1269f8b94bf6bdb0f95718da008e14ed
Matteo Montalbetti <matteo.montalbetti3@gmail.com>
2025-02-14 15:15:19 +0100

    removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
 
